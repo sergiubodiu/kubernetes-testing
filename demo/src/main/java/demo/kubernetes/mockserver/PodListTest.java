205e9d3f5ba79fc96bc74c0f23852d39d74f7e89
Sergiu Bodiu <sergiubodiu@users.noreply.github.com>
2023-04-05 18:14:19 +0800

    writing crud tests

---
+++ demo/src/main/java/demo/kubernetes/mockserver/PodListTest.java
@@ -0,0 +1,19 @@
+package demo.kubernetes.mockserver;
+
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+
+public class PodListTest {
+    public static void main(String[] args) {
+        try (KubernetesClient client = new KubernetesClientBuilder().build()) {
+            client.pods().inAnyNamespace().list()
+                .getItems()
+                .stream()
+                .map(Pod::getMetadata)
+                .map(ObjectMeta::getName)
+                .forEach(System.out::println);
+        }
+    }
+}
